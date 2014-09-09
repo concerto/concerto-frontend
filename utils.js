d555a8f993eaa4bb02435f7133d87e2272f2223a
Brian Michalski <bmichalski@gmail.com>
2014-09-08 23:45:15 -0400

    Add a baseURL parameter to carry across the protocol + domain + path that should be used everywhere.

---
+++ utils.js
@@ -0,0 +1,13 @@
+/**
+ * Prepend a base path to a URL as needed.
+ *
+ * If a base is supplied and the URL is not absolute, prepend the base to
+ * return an absolute URL.
+ */
+PolymerExpressions.prototype.resolveURL = function(url, base) {
+  var r = new RegExp('^(?:[a-z]+:)?//', 'i');
+  if (!base || r.test(url)) {
+    return url;
+  }
+  return base + url;
+};
