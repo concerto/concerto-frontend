eff2470c83e11a6dc675bb26b73d50cf76f35ca2
Marvin Frederickson <marvin.frederickson@gmail.com>
2015-10-05 10:40:38 -0800

    auto strip html from vulcanized output

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-// Wrapper function to encapsulate Grunt config
+// Wrapper function to encapsulate Grunt config 
 module.exports = function(grunt) {
   // Initialize our Grunt config object
   grunt.initConfig({
@@ -19,6 +19,31 @@ module.exports = function(grunt) {
         }
       }
     },
+    "regex-replace": {
+      componentize: {
+        src: ['concerto-frontend-gem/app/assets/html/concerto_frontend/concerto-frontend.html'],
+        actions: [
+          {
+            name: 'stripstart',
+            search: /^((?:\s|.)*?)(<script>)/,
+            replace: '$2',
+            flags: ''
+          },
+          {
+            name: 'stripvulcan',
+            search: '(</head.*?>)(<dom-module)',
+            replace: '$2',
+            flags: ''
+          },
+          {
+            name: 'stripend',
+            search: '(</dom-module>)(.*?</html>)',
+            replace: '$1',
+            flags: ''
+          }
+        ]
+      }
+    },
     // Read NPM package settings and store values in pkg property
     pkg: grunt.file.readJSON("package.json"),
     // Configure the vulcanize plugin to concatenate our Polymer html files
@@ -37,7 +62,7 @@ module.exports = function(grunt) {
     watch: {
       scripts: {
         files: ['*.html', 'contents/*.html'],
-        tasks: ['vulcanize']
+        tasks: ['vulcanize', 'regex-replace']
       },
     },
   });
@@ -47,6 +72,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-vulcanize'); // concatenate Polymer elements
   grunt.loadNpmTasks('grunt-contrib-watch'); // run tasks when files are modified
   grunt.loadNpmTasks('grunt-bowercopy'); // copy files to gem
+  grunt.loadNpmTasks('grunt-regex-replace');
 
   // Set up our default task that is run when 'grunt' is executed
   grunt.registerTask('default', ['vulcanize', 'bowercopy']);
